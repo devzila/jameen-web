692c585c4e921be6d770c4bf2f38179c9e3bd4a2
nilay <nilayanand@gmail.com>
2023-07-16 19:33:18 +0530

    pagination component

---
+++ src/components/Pagination.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { CPagination, CPaginationItem } from '@coreui/react'
+import PropTypes from 'prop-types'
+
+const Pagination = ({ path, current, pageCount }) => {
+  const accommodatableLimit = 10
+  const trailItemsCount = 2
+  let trailingLabel = ''
+  return (
+    <CPagination aria-label="Page navigation example">
+      {current > 1 && (
+        <CPaginationItem aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </CPaginationItem>
+      )}
+      {[...Array(Math.min(pageCount, accommodatableLimit))].map((x, i) => (
+        <CPaginationItem key={i} {...(i + 1 == current ? { active: true } : {})}>
+          {i + 1}
+        </CPaginationItem>
+      ))}
+      {pageCount > accommodatableLimit &&
+        [...Array(Math.min(pageCount - accommodatableLimit, trailItemsCount + 1))].map((x, i) => (
+          <CPaginationItem
+            key={pageCount - trailItemsCount + i}
+            {...(pageCount - trailItemsCount + i == current ? { active: true } : {})}
+          >
+            {pageCount - trailItemsCount + i}
+          </CPaginationItem>
+        ))}
+      {current < pageCount && (
+        <CPaginationItem aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </CPaginationItem>
+      )}
+    </CPagination>
+  )
+}
+Pagination.propTypes = {
+  path: PropTypes.string,
+  current: PropTypes.number,
+  pageCount: PropTypes.number,
+}
+export default Pagination
