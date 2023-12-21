1c455855586445ba66d1daf05922017ade7beb41
wizrahull <purirahul31@gmail.com>
2023-12-21 12:31:53 +0530

    assigned prop

--- src/views/settings/User.js
+++ src/views/settings/User.js
@@ -1,65 +1,108 @@
-import React, { useEffect, useState } from 'react'
-import { Container, Row, Button, Col, Card, Table } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
 import useFetch from 'use-http'
-import UserForm from './UserForm'
+import Paginate from '../../components/Pagination'
+import { useNavigate } from 'react-router-dom'
+import { BsThreeDots } from 'react-icons/bs'
+import { Dropdown } from 'react-bootstrap'
+import CustomDivToggle from 'src/components/CustomDivToggle'
+import { Link, useParams } from 'react-router-dom'
 
-export default function User() {
-  const { get, response } = useFetch()
-  const [users, setUsers] = useState([])
-  const [user_properties, setUser_properties] = useState([])
+// react-bootstrap components
+import { Badge, Button, Card, Navbar, Nav, Table, Container, Row, Col } from 'react-bootstrap'
 
+function Index() {
+  const { companyId } = useParams()
+  const [users, setusers] = useState([])
+  const [pagination, setPagination] = useState(null)
+  const [currentPage, setCurrentPage] = useState(1)
+  const { get, post, response, loading, error } = useFetch()
   useEffect(() => {
-    loadInitialUsers()
-  }, [])
+    loadInitialusers()
+  }, [currentPage])
+  const history = useNavigate()
+  const addUser = () => {
+    history(`/companies/${companyId}/users/add`)
+  }
 
-  async function loadInitialUsers() {
-    const endpoint = `/v1/admin/users/`
-    const initialUser = await get(endpoint)
+  async function loadInitialusers() {
+    const initialusers = await get(`/v1/admin/users?page=${currentPage}`)
     if (response.ok) {
-      setUsers(initialUser.data.users)
-      console.log(initialUser.data.users)
-
-      setUser_properties(initialUser.data.users.assigned_properties)
-      console.log(user_properties)
+      setusers(initialusers.data.users)
+      setPagination(initialusers.data.pagination)
     }
   }
+
+  function handlePageClick(e) {
+    setCurrentPage(e.selected + 1)
+  }
+
   return (
     <>
-      <UserForm />
-      <Container fluid>
+      {error && error.Error}
+      {loading && 'Loading...'}
+
+      <Container className="container-fluid">
         <Row>
           <Col md="12">
-            <Card className="strpied-tabled-with-hover ">
+            <Card className="table">
               <Card.Header>
                 <Row>
-                  <Col md="8">
-                    <Card.Title as="h4"> User </Card.Title>
+                  <Col md="12">
+                    <Card.Title as="h4"> Users </Card.Title>
+                    <p className="card-category">List of Users </p>
                   </Col>
                   <Col md="4" className="align-right">
-                    {/* Use the Search component here */}
+                    <Button onClick={addUser}>Add User</Button>
                   </Col>
                 </Row>
               </Card.Header>
               <Card.Body className="table-full-width table-responsive px-0">
-                <Table className="table-hover table-striped">
+                <Table className="table">
                   <thead>
                     <tr>
-                      <th className="border-0">NAME</th>
-                      <th className="border-0">EMAIL</th>
-                      <th className="border-0">MOBILE NO.</th>
-                      <th className="border-0">Role</th>
+                      <th className="border-0">Name</th>
+                      <th className="border-0">Email</th>
+                      <th className="border-0">Phone Number</th>
+                      <th className="border-0">Username</th>
+                      <th className="border-0">Role ID</th>
                       <th className="border-0">Assigned Properties</th>
                     </tr>
                   </thead>
-
                   <tbody>
-                    {users.map((User) => (
-                      <tr key={User.id}>
-                        <td>{User.name}</td>
-                        <td>{User.email}</td>
-                        <td>{User.mobile_number}</td>
-                        <td>{User.role.name}</td>
-                        <td></td>
+                    {users.map((user) => (
+                      <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.mobile_number}</td>
+                        <td>{user.username}</td>
+                        <td>{user.role.name}</td>
+                        <td>
+                          {user.assigned_properties.map((property) => property.name).join(', ')}
+                        </td>
+                        <td>
+                          <Dropdown key={user.id}>
+                            <Dropdown.Toggle as={CustomDivToggle} style={{ cursor: 'pointer' }}>
+                              <BsThreeDots />
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                              <Dropdown.Item
+                                key={`edit-${user.id}`}
+                                as={Link}
+                                to={`/companies/${companyId}/users/${user.id}/edit`}
+                              >
+                                Edit
+                              </Dropdown.Item>
+
+                              <Dropdown.Item
+                                key={`user-show-${user.id}`}
+                                as={Link}
+                                to={`/companies/${companyId}/users/${user.id}`}
+                              >
+                                User Show
+                              </Dropdown.Item>
+                            </Dropdown.Menu>
+                          </Dropdown>
+                        </td>
                       </tr>
                     ))}
                   </tbody>
@@ -68,7 +111,23 @@ export default function User() {
             </Card>
           </Col>
         </Row>
+        <Row>
+          <Col md="12">
+            {pagination ? (
+              <Paginate
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={pagination.per_page}
+                pageCount={pagination.total_pages}
+                forcePage={currentPage - 1}
+              />
+            ) : (
+              <br />
+            )}
+          </Col>
+        </Row>
       </Container>
     </>
   )
 }
+
+export default Index
