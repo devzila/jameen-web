d4191a43c3eca2c9389ea89d0df39d5760f2a36e
wizrahull <purirahul31@gmail.com>
2023-12-26 15:39:54 +0530

    add user

--- src/components/MultiValueListPop.js
+++ src/components/MultiValueListPop.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { CTooltip, CButton } from '@coreui/react'
 
-export default function AssignedPropertiesPop(properties) {
-  const assigned_properties = properties.prop.map((val) => val.name)
+export default function MultiValueListPop(list) {
+  const list_values = list.prop.map((val) => val.name)
 
   return (
     <div>
       <CTooltip
-        content={assigned_properties}
+        content={list_values}
         placement="top"
         style={{ overflow: 'hidden' }}
         className="overflow-hidden"
       >
         <CButton variant="ghost" color="dark">
-          {assigned_properties[0]},...
+          {list_values[0]},...
         </CButton>
       </CTooltip>
     </div>
