881bbbb19bdde78475d0ed8b1ebf613e7860187f
wizrahull <purirahul31@gmail.com>
2024-01-23 15:18:54 +0530

    roles show module

--- src/views/settings/Role/ShowRoles.js
+++ src/views/settings/Role/ShowRoles.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import useFetch from 'use-http'
 import PropTypes from 'prop-types'
+import { Button, Form, Row, Col } from 'react-bootstrap'
+import CIcon from '@coreui/icons-react'
+import { cilCheck, cilX } from '@coreui/icons'
+import logo from '../../../assets/images/jameen-logo.png'
 
 import {
   CButton,
@@ -15,6 +19,9 @@ export default function ShowRoles({ roleId }) {
   const [visible, setVisible] = useState(false)
   const [role, setRole] = useState([])
   const { get, response } = useFetch()
+
+  const renderIcon = (condition) =>
+    condition ? <CIcon icon={cilCheck} size="xl" /> : <CIcon icon={cilX} size="xl" />
 
   useEffect(() => {
     getUserData()
@@ -58,37 +65,16 @@ export default function ShowRoles({ roleId }) {
         </CModalHeader>
         <CModalBody>
           <CContainer>
+            <p className="text-center display-6" style={{ color: '#00bfcc' }}>
+              JAMEEN
+            </p>
             <div className="container bootstrap snippets bootdey">
               <div className="panel-body inf-content">
                 <div className="row">
-                  <div className="col-md-4">
-                    <div className="col text-center">
-                      <img
-                        alt="Avatar Image"
-                        style={{
-                          width: '300px',
-                          height: '300px',
-
-                          marginTop: '2%',
-                          marginLeft: '4%',
-                          borderRadius: '50%',
-                        }}
-                        title="Avatar"
-                        className="img-circle img-thumbnail isTooltip  "
-                        src={'https://bootdey.com/img/Content/avatar/avatar7.png'}
-                        data-original-title="Usuario"
-                      />
-                    </div>
-                    <ul title="Ratings" className="list-inline ratings text-center">
-                      <li>
-                        <span className="glyphicon glyphicon-star"></span>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="col-md-7">
+                  <div className="col">
                     <br />
                     <div className="table-responsive">
-                      <table className="table table-user-information">
+                      <table className="mt-4 table table-user-information">
                         <tbody>
                           <tr>
                             <td>
@@ -109,15 +95,6 @@ export default function ShowRoles({ roleId }) {
                             <td className="text-primary text-black-50">
                               {role.description || '-'}
                             </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              <strong>
-                                <span className="glyphicon glyphicon-bookmark text-primary"></span>
-                                Privileges
-                              </strong>
-                            </td>
-                            {/* <td className="text-primary text-black-50">{role.privileges || '-'}</td> */}
                           </tr>
 
                           <tr>
@@ -160,6 +137,96 @@ export default function ShowRoles({ roleId }) {
                     </div>
                   </div>
                 </div>
+                <Row>
+                  <strong>
+                    <span className="glyphicon glyphicon-bookmark text-primary"></span>
+                    Privileges
+                  </strong>
+                  <Col className="pr-3 mt-1" md="6">
+                    <div className="card" style={{ margin: '20px ' }}>
+                      <div className="card-header p-3">User</div>
+                      <div className="card-body">
+                        <ul className="list-group list-group-flush">
+                          <li className="list-group-item ">
+                            View
+                            <p className="checkbox">{renderIcon(role?.privileges?.users?.view)}</p>
+                          </li>
+                          <li className="list-group-item">
+                            Create
+                            <p className="checkbox">
+                              {renderIcon(role?.privileges?.users?.create)}
+                            </p>
+                          </li>
+                          <li className="list-group-item">
+                            Delete
+                            <p className="checkbox">
+                              {renderIcon(role?.privileges?.users?.delete)}
+                            </p>
+                          </li>
+                          <li className="list-group-item">
+                            Update
+                            <p className="checkbox">
+                              {renderIcon(role?.privileges?.users?.update)}
+                            </p>
+                          </li>
+                          <li className="list-group-item">
+                            Add Notes
+                            <p className="checkbox">
+                              {renderIcon(role?.privileges?.users?.add_notes)}
+                            </p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </Col>
+                  <Col md="6" className="mt-1">
+                    <div className="card" style={{ margin: '20px ' }}>
+                      <div className="card-header p-3">Maintenance</div>
+                      <div className="card-body">
+                        <ul className="list-group list-group-flush">
+                          <li className="list-group-item">
+                            View
+                            <p className="checkbox">
+                              {renderIcon(role?.privileges?.maintenance?.view)}
+                            </p>
+                          </li>
+                          <li className="list-group-item">
+                            Create
+                            <p className="checkbox">
+                              {renderIcon(role?.privileges?.maintenance?.create)}
+                            </p>
+                          </li>
+                          <li className="list-group-item">
+                            Delete
+                            <p className="checkbox">
+                              {renderIcon(role?.privileges?.maintenance?.delete)}
+                            </p>
+                          </li>
+                          <div className="m-4 list-group-item">
+                            <label>
+                              <span className=" default"></span>
+                            </label>
+                          </div>
+                        </ul>
+                      </div>
+                    </div>
+                  </Col>
+                  <Col>
+                    <div className="card" style={{ margin: '20px ' }}>
+                      <div className="card-header p-3">Setting</div>
+                      <div className="card-body">
+                        <ul className="list-group list-group-flush">
+                          <li className="list-group-item">
+                            Manage
+                            <p className="checkbox">
+                              {renderIcon(role?.privileges?.settings?.manage)}
+                            </p>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
                 <div className="text-center">
                   <CModalFooter>
                     <CButton color="secondary" onClick={() => setVisible(false)}>
